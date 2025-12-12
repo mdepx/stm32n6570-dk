2f393f0bf252bc1792acecbcc917d1440cbe37da
Ruslan Bukin <br@bsdpad.com>
2025-12-12 17:16:24 +0000

    setup ltdc, xspi configuration

--- src/board.c
+++ src/board.c
@@ -41,12 +41,16 @@
 static struct stm32n6_rcc_softc rcc_sc;
 static struct stm32l4_usart_softc usart_sc;
 static struct stm32f4_timer_softc timer_sc;
+static struct stm32n6_xspi_softc xspi1_sc;
+static struct stm32n6_pwr_softc pwr_sc;
 
 struct stm32f4_gpio_softc gpio_sc;
 struct stm32f4_ltdc_softc ltdc_sc;
 
 static struct arm_nvic_softc nvic_sc;
 static struct mdx_device dev_nvic = { .sc = &nvic_sc };
+
+static struct layer_info info;
 
 void
 udelay(uint32_t usec)
@@ -78,41 +82,65 @@ static const struct stm32_gpio_pin uart_pins[] = {
 	{ PORT_D, 5, MODE_ALT, 7, OT_PP, OS_H, FLOAT }, /* D1 USART2_TX */
 	{ PORT_F, 6, MODE_ALT, 7, OT_PP, OS_H, FLOAT }, /* D0 USART2_RX */
 
-	{ PORT_G,  0, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* R0 */
-	{ PORT_D,  9, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* R1 */
-	{ PORT_D, 15, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* R2 */
-	{ PORT_B,  4, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* R3 */
-	{ PORT_H,  4, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* R4 */
-	{ PORT_A, 15, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* R5 */
-	{ PORT_G, 11, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* R6 */
-	{ PORT_D,  8, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* R7 */
+	/* ROCKTEK RK050HR18-CTG */
+	{ PORT_G,  0, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* R0 */
+	{ PORT_D,  9, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* R1 */
+	{ PORT_D, 15, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* R2 */
+	{ PORT_B,  4, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* R3 */
+	{ PORT_H,  4, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* R4 */
+	{ PORT_A, 15, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* R5 */
+	{ PORT_G, 11, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* R6 */
+	{ PORT_D,  8, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* R7 */
 
-	{ PORT_G, 12, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* G0 */
-	{ PORT_G,  1, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* G1 */
-	{ PORT_A,  1, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* G2 */
-	{ PORT_A,  0, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* G3 */
-	{ PORT_B, 15, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* G4 */
-	{ PORT_B, 12, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* G5 */
-	{ PORT_B, 11, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* G6 */
-	{ PORT_G,  8, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* G7 */
+	{ PORT_G, 12, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* G0 */
+	{ PORT_G,  1, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* G1 */
+	{ PORT_A,  1, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* G2 */
+	{ PORT_A,  0, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* G3 */
+	{ PORT_B, 15, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* G4 */
+	{ PORT_B, 12, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* G5 */
+	{ PORT_B, 11, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* G6 */
+	{ PORT_G,  8, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* G7 */
 
-	{ PORT_G, 15, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* B0 */
-	{ PORT_A,  7, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* B1 */
-	{ PORT_B,  2, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* B2 */
-	{ PORT_G,  6, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* B3 */
-	{ PORT_H,  3, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* B4 */
-	{ PORT_H,  6, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* B5 */
-	{ PORT_A,  8, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* B6 */
-	{ PORT_A,  2, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* B7 */
+	{ PORT_G, 15, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* B0 */
+	{ PORT_A,  7, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* B1 */
+	{ PORT_B,  2, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* B2 */
+	{ PORT_G,  6, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* B3 */
+	{ PORT_H,  3, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* B4 */
+	{ PORT_H,  6, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* B5 */
+	{ PORT_A,  8, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* B6 */
+	{ PORT_A,  2, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* B7 */
 
-	{ PORT_G, 13, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* LCD_DE */
-	{ PORT_Q,  3, MODE_OUT,  0, OT_PP, OS_H, FLOAT }, /* LCD_ON/OFF */
-	{ PORT_B, 14, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* LCD_HSYNC */
-	{ PORT_E, 11, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* LCD_VSYNC */
-	{ PORT_B, 13, MODE_ALT, 14, OT_PP, OS_H, FLOAT }, /* LCD_CLK */
-	{ PORT_Q,  4, MODE_INP,  0, OT_PP, OS_H, PULLUP }, /* LCD_INT */
-	{ PORT_Q,  6, MODE_OUT,  0, OT_PP, OS_H, FLOAT }, /* LCD_BL_CTRL */
-	{ PORT_E,  1, MODE_OUT,  0, OT_PP, OS_H, FLOAT }, /* NRST */
+	{ PORT_G, 13, MODE_OUT,  0, OT_PP, OS_VH, FLOAT }, /* LCD_DE */
+	{ PORT_Q,  3, MODE_OUT,  0, OT_PP, OS_VH, FLOAT }, /* LCD_ON/OFF */
+	{ PORT_B, 14, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* LCD_HSYNC */
+	{ PORT_E, 11, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* LCD_VSYNC */
+	{ PORT_B, 13, MODE_ALT, 14, OT_PP, OS_VH, FLOAT }, /* LCD_CLK */
+	{ PORT_Q,  4, MODE_INP,  0, OT_PP, OS_VH, PULLUP }, /* LCD_INT */
+	{ PORT_Q,  6, MODE_OUT,  0, OT_PP, OS_VH, FLOAT }, /* LCD_BL_CTRL */
+	{ PORT_E,  1, MODE_OUT,  0, OT_PP, OS_VH, FLOAT }, /* NRST */
+
+	/* XSPIM_P1 APS256XXN-OBR-BG */
+	{ PORT_O,  0, MODE_ALT, 9, OT_PP, OS_VH, PULLUP }, /* NCS1 */
+	{ PORT_O,  2, MODE_ALT, 9, OT_PP, OS_VH, PULLUP }, /* DQS0 */
+	{ PORT_O,  3, MODE_ALT, 9, OT_PP, OS_VH, PULLUP }, /* DQS1 */
+	{ PORT_O,  4, MODE_ALT, 9, OT_PP, OS_VH, PULLUP }, /* CLK */
+
+	{ PORT_P,  0, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO0 */
+	{ PORT_P,  1, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO1 */
+	{ PORT_P,  2, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO2 */
+	{ PORT_P,  3, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO3 */
+	{ PORT_P,  4, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO4 */
+	{ PORT_P,  5, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO5 */
+	{ PORT_P,  6, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO6 */
+	{ PORT_P,  7, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO7 */
+	{ PORT_P,  8, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO8 */
+	{ PORT_P,  9, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO9 */
+	{ PORT_P, 10, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO10 */
+	{ PORT_P, 11, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO11 */
+	{ PORT_P, 12, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO12 */
+	{ PORT_P, 13, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO13 */
+	{ PORT_P, 14, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO14 */
+	{ PORT_P, 15, MODE_ALT, 9, OT_PP, OS_VH, FLOAT }, /* IO15 */
 
 	PINS_END
 };
@@ -121,17 +149,21 @@ void
 board_init(void)
 {
 	struct rcc_config cfg;
-
 	bzero(&cfg, sizeof(struct rcc_config));
 	cfg.ahb4enr = AHB4ENSR_GPIOAEN | AHB4ENSR_GPIOBEN | AHB4ENSR_GPIOCEN |
 	    AHB4ENSR_GPIODEN | AHB4ENSR_GPIOEEN | AHB4ENSR_GPIOFEN |
 	    AHB4ENSR_GPIOGEN | AHB4ENSR_GPIOHEN | AHB4ENSR_GPIONEN |
-	    AHB4ENSR_GPIOOEN | AHB4ENSR_GPIOPEN | AHB4ENSR_GPIOQEN;
+	    AHB4ENSR_GPIOOEN | AHB4ENSR_GPIOPEN | AHB4ENSR_GPIOQEN |
+	    AHB4ENSR_PWREN;
+	cfg.ahb5enr = AHB5ENSR_NPUENS | AHB5ENSR_CACHEAXIENS |
+	    AHB5ENSR_GPU2DENS | AHB5ENSR_GFXMMUENS | AHB5ENSR_XSPI3ENS |
+	    AHB5ENSR_XSPIMENS | AHB5ENSR_XSPI2ENS | AHB5ENSR_XSPI1ENS |
+	    AHB5ENSR_FMCENS | AHB5ENSR_JPEGENS | AHB5ENSR_DMA2DENS;
 	cfg.apb1lenr = APB1LENSR_UART5ENS | APB1LENSR_USART2ENS |
 	    APB1LENSR_TIM2ENS;
 	cfg.apb2enr = APB2ENSR_USART1EN | APB2ENSR_TIM1ENS;
-	cfg.apb5enr = AHB5ENSR_LTDCEN | AHB5ENSR_DCMIPPENS |
-	    AHB5ENSR_GFXTIMENS | AHB5ENSR_VENCENS | AHB5ENSR_CSIENS;
+	cfg.apb5enr = APB5ENSR_LTDCEN | APB5ENSR_DCMIPPENS |
+	    APB5ENSR_GFXTIMENS | APB5ENSR_VENCENS | APB5ENSR_CSIENS;
 
 	/* RCC */
 	stm32n6_rcc_init(&rcc_sc, RCC_BASE);
@@ -147,14 +179,37 @@ board_init(void)
 
 	/* NVIC */
 	arm_nvic_init(&dev_nvic, NVIC_BASE);
-	*(uint32_t *)0xe000ed08 = 0x34180000; /* Setup vtor */
+	*(uint32_t *)0xe000ed08 = 0x34120000; /* Setup VTOR. */
 
 	/* TIMER */
 	stm32f4_timer_init(&timer_sc, TIM1_BASE, 64000000);
 	mdx_intc_setup(&dev_nvic, 115, stm32f4_timer_intr, &timer_sc);
 	mdx_intc_enable(&dev_nvic, 115);
 
+	/* PSRAM */
+	stm32n6_pwr_init(&pwr_sc, PWR_BASE);
+	stm32n6_xspi_init(&xspi1_sc, XSPI1_BASE);
+	stm32n6_xspi_setup(&xspi1_sc, NULL);
+
+	info.width = 800;
+	info.height = 480;
+	info.hsync = 4;
+	info.vsync = 4;
+	info.vfp = 8;
+	info.hfp = 8;
+	info.vbp = 8;
+	info.hbp = 8;
+	info.bpp = 24;
+	info.base = 0x90000000;
+	info.base = 0x34200000;
+
+	pin_set(&gpio_sc, PORT_E,  1, 1); /* NRST */
+	pin_set(&gpio_sc, PORT_Q,  3, 1); /* LCD_ON/OFF */
+	pin_set(&gpio_sc, PORT_Q,  6, 1); /* LCD_BL_CTRL */
+	pin_set(&gpio_sc, PORT_G, 13, 1); /* LCD_DE */
+
 	stm32f4_ltdc_init(&ltdc_sc, LTDC_BASE);
+	stm32f4_ltdc_setup(&ltdc_sc, &info, 1);
 
 #if 0
 	malloc_init();
