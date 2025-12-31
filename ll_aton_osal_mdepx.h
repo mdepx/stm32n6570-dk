c827eaef6916dca17c307cac654ab5d16a55e77a
Ruslan Bukin <br@bsdpad.com>
2025-12-31 22:29:01 +0000

    implement aton critical section enter/exit

--- ll_aton_osal_mdepx.h
+++ ll_aton_osal_mdepx.h
@@ -1,5 +1,9 @@
+#include <sys/systm.h>
+
 void npu_setup_irq(int, void *);
 
+#define	LL_ATON_OSAL_ENTER_CS()		critical_enter();
+#define	LL_ATON_OSAL_EXIT_CS()		critical_exit();
 #define	LL_ATON_OSAL_INIT()
 #define	LL_ATON_OSAL_DEINIT()
 #define	LL_ATON_OSAL_WFE()		__WFE()
