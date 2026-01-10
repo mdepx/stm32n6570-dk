531e28f5651e1b80cef33fd6cd981f220961dc80
Ruslan Bukin <br@bsdpad.com>
2026-01-10 20:09:00 +0000

    add macro

--- ll_aton_osal_mdepx.h
+++ ll_aton_osal_mdepx.h
@@ -10,3 +10,5 @@ void npu_setup_irq(int, void *);
 #define	LL_ATON_OSAL_SIGNAL_EVENT()
 #define	LL_ATON_OSAL_INSTALL_IRQ(irq_aton_line_nr, handler)	\
     npu_setup_irq(irq_aton_line_nr, handler)
+#define	LL_ATON_OSAL_LOCK_NPU_CACHE()
+#define	LL_ATON_OSAL_UNLOCK_NPU_CACHE()
